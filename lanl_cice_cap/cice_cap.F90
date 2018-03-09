c58eedca2e39d7784084778eed494d376af83487
Xingren Wu <Xingren.Wu@noaa.gov>
2018-03-09 15:20:43 -0500

    update for cice_mx025 and bug fix for dump file output
    Change-Id: I8767af15f826cae47ec7c63137c6350bcba20747

--- lanl_cice_cap/cice_cap.F90
+++ lanl_cice_cap/cice_cap.F90
@@ -1764,7 +1764,7 @@ module cice_cap_mod
 
     type(ESMF_Field)         :: field
     real(ESMF_KIND_R8), dimension(:,:), pointer  :: f2d
-    integer                  :: rc
+    integer                  :: i,j,rc
 
     if(.not. write_diagnostics) return ! remove this line to debug field connection
 
@@ -1782,7 +1782,12 @@ module cice_cap_mod
       file=__FILE__)) &
       return  ! bail out
 
-    f2d(:,:) = farray(:,:,1)
+    !f2d(:,:) = farray(:,:,1)
+    do j = lbound(f2d,2),ubound(f2d,2)
+     do i = lbound(f2d,1),ubound(f2d,1)
+      f2d(i,j) = farray(i+1,j+1,1)
+     enddo
+    enddo
 
     call ESMF_FieldWrite(field, fileName='field_ice_internal_'//trim(stdname)//'.nc', &
       timeslice=slice, rc=rc) 
